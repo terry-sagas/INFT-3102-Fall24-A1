51261bb63fe0e4abbcf0435b0779443fde187fc3
Terry Sagas <terrysagas2003@gmail.com>
2024-09-27 02:23:52 -0400

    added logo

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addCollection("games", function() {
         return require("./src/_data/games.json").games;
     });
+    eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/gameicons");
     return {
         dir: {
